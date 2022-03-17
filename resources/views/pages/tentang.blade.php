185394fb54782dfb92fc45e1903efd4253ecbfee
cucibaju123 <cucibaju123@gmail.com>
2022-03-17 17:55:31 +0700

    Deleted unnecessary filesa

--- resources/views/pages/tentang.blade.php
+++ resources/views/pages/tentang.blade.php
@@ -85,7 +85,7 @@
         <div class="row">
           <div class="col-lg-4 d-flex align-items-stretch">
             <div class="content">
-              <h3>About Application</h3>
+              <h3>Tentang Aplikasi</h3>
               <p>
                 Aplikasi SIPEPSI merupakan aplikasi berbasis web yang termasuk ke dalam sistem pakar menggunakan metode certainty factor dan Bahasa Pemrograman PHP dengan Framework Laravel.
               </p>
@@ -97,21 +97,21 @@
                 <div class="col-xl-4 d-flex align-items-stretch">
                   <div class="icon-box mt-4 mt-xl-0">
                     <i class="fas fa-disease"></i>
-                    <h4>Epilepsy</h4>
+                    <h4>Penyakit Epilepsi</h4>
                     <p>Epilepsi merupakan kumpulan gejala atau tanda klinis yang muncul disebabkan gangguan fungsi otak secara intermiten, yang terjadi akibat lepasnya muatan listrik abnormal atau berlebihan.</p>
                   </div>
                 </div>
                 <div class="col-xl-4 d-flex align-items-stretch">
                   <div class="icon-box mt-4 mt-xl-0">
                     <i class="fab fa-ubuntu"></i>
-                    <h4>Expert System</h4>
+                    <h4>Sistem Pakar</h4>
                     <p>Expert system atau Sistem pakar merupakan suatu aplikasi komputer yang bertujuan untuk memecahkan persoalan di bidang tertentu dengan mengadopsi pengetahuan dari seorang pakar.</p>
                   </div>
                 </div>
                 <div class="col-xl-4 d-flex align-items-stretch">
                   <div class="icon-box mt-4 mt-xl-0">
                     <i class="fas fa-industry"></i>
-                    <h4>Certainty Factor</h4>
+                    <h4>Algoritma Certainty Factor</h4>
                     <p>Certainty factor merupakan nilai parameter klinis yang diberikan oleh MYCIN untuk menunjukkan besarnya kepercayaan. Metode ini diperkenalkan oleh Shortlife Buchanan.</p>
                   </div>
                 </div>
@@ -123,200 +123,30 @@
       </div>
     </section><!-- End Why Us Section -->
 
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services">
-      <div class="container">
-
-        {{-- <div class="section-title">
-          <h2></h2>
-          <p></p>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="icon-box">
-              <div class="icon"><i class="icofont-heart-beat"></i></div>
-              <h4><a href=""></a></h4>
-              <p></p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div class="icon-box">
-              <div class="icon"><i class="icofont-prescription"></i></div>
-              <h4><a href=""></a></h4>
-              <p></p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-            <div class="icon-box">
-              <div class="icon"><i class="icofont-connection"></i></div>
-              <h4><a href=""></a></h4>
-              <p></p>
-            </div>
-          </div>
-        </div> --}}
-
+   <!-- ======= Doctors Section ======= -->
+   <section id="doctors" class="doctors">
+    <div class="container">
+      <div class="section-title">
+        <h2>Tenaga Ahli</h2>
+        <p>Sipepsi merupakan aplikasi pendiagnosa yang telah disetujui oleh tenaga ahli penyakit saraf.</p>
       </div>
-    </section><!-- End Services Section -->
-
-    <!-- ======= Doctors Section ======= -->
-    <section id="doctors" class="doctors">
-      <div class="container">
-        <div class="section-title">
-          <h2>Quotes</h2>
-          <p>Kumpulan kata yang menginspirasi developer dalam pembuatan Aplikasi SIPEPSI.</p>
-        </div>
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="member d-flex align-items-start">
-              <div class="pic"><img src="assets/img/doctors/doctors-1.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Bill Gates</h4>
-                <span>Pendiri Microsoft</span>
-                <p>“It’s fine to celebrate success but it is more important to heed the lessons of failure</p>
-                <div class="social">
-                  <a href=""><i class="ri-twitter-fill"></i></a>
-                  <a href=""><i class="ri-facebook-fill"></i></a>
-                  <a href=""><i class="ri-instagram-fill"></i></a>
-                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
-              </div>
+          <div class="member d-flex justify-content-center">
+            <div class="py-5 text-center">
+              <p class="pb-5">disetujui oleh</p>
+              <h3>dr.Danny Sp.S</h3>
+              <p>Dokter Spesialis Saraf</p>
+              <hr>
+              <p>RSUD dr.Slamet Garut</p>
             </div>
+         
+            {{-- <div class="member-info">
+              <h4>dr.Danny Sp.S</h4>
+              <span>Dokter Spesialis Saraf<br/>RSUD dr.Slamet Garut</span>
+            </div> --}}
           </div>
 
-          <div class="col-lg-6 mt-4 mt-lg-0">
-            <div class="member d-flex align-items-start">
-              <div class="pic"><img src="assets/img/doctors/doctors-2.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Walt Disney</h4>
-                <span>Produser Film</span>
-                <p>“Why worry? If you’ve done the very best you can, then worrying won’t make it any better.”</p>
-                <div class="social">
-                  <a href=""><i class="ri-twitter-fill"></i></a>
-                  <a href=""><i class="ri-facebook-fill"></i></a>
-                  <a href=""><i class="ri-instagram-fill"></i></a>
-                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 mt-4">
-            <div class="member d-flex align-items-start">
-              <div class="pic"><img src="assets/img/doctors/doctors-3.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Steve Jobs</h4>
-                <span>Pendiri Apple</span>
-                <p>"The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle."</p>
-                <div class="social">
-                  <a href=""><i class="ri-twitter-fill"></i></a>
-                  <a href=""><i class="ri-facebook-fill"></i></a>
-                  <a href=""><i class="ri-instagram-fill"></i></a>
-                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-6 mt-4">
-            <div class="member d-flex align-items-start">
-              <div class="pic"><img src="assets/img/doctors/doctors-4.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Mark Zuckerberg</h4>
-                <span>Pendiri Facebook</span>
-                <p>"The biggest risk is not taking any risk. In a world that changing really quickly, the only strategy that is guaranteed to fail is not taking risks."</p>
-                <div class="social">
-                  <a href=""><i class="ri-twitter-fill"></i></a>
-                  <a href=""><i class="ri-facebook-fill"></i></a>
-                  <a href=""><i class="ri-instagram-fill"></i></a>
-                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Doctors Section -->
-
-    <!-- ======= Testimonials Section ======= -->
-    <section id="testimonials" class="testimonials">
-      <div class="container">
-
-        <div class="owl-carousel testimonials-carousel">
-
-          <div class="testimonial-wrap">
-            <div class="testimonial-item">
-              <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-              <h3>dr. H. R.M. Willy Indrawilis, Sp.KJ</h3>
-              <h4>Dokter Spesialis Jiwa (Psikiatri)</h4>
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                "Jangan pernah menunda pekerjaan."
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-
-          <div class="testimonial-wrap">
-            <div class="testimonial-item">
-              <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-              <h3>dr. Johnson Manurung, Sp.PD</h3>
-              <h4>Dokter Spesialis Penyakit Dalam</h4>
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                "Kerjakan apa yang bisa dilakukan sekarang"
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-
-          <div class="testimonial-wrap">
-            <div class="testimonial-item">
-              <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-              <h3>dr. Yanti Widamayanti, Sp.PD</h3>
-              <h4>Dokter Spesialis Penyakit Dalam</h4>
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                "Pengabdian dan ketenangan merupakan kunci dari kehidupan"
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-
-          <div class="testimonial-wrap">
-            <div class="testimonial-item">
-              <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-              <h3>dr. Ridwan Sofyansyah, Sp.JP</h3>
-              <h4>Dokter Spesialis Jantung dan Pembuluh Darah</h4>
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                "Gunakan masa mudamu sebagai pengalaman dan jatah dari kegagalanmu"
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-
-          <div class="testimonial-wrap">
-            <div class="testimonial-item">
-              <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-              <h3></h3>
-              <h4>dr. Hadiyana Suryadi, Sp.B</h4>
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                "Belajar dari pengalamanmu dan pengalaman orang lain."
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Testimonials Section -->
+    </div>
+  </section><!-- End Doctors Section -->
 
   </main><!-- End #main -->
 
@@ -334,13 +164,6 @@
           <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/ -->
           Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div> --}}
-      </div>
-      <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </footer><!-- End Footer -->
